ebb33a96bb8d3de053391c837d33329e47c514e8
Admin <Admin@DESKTOP-89UFM61>
2023-09-21 14:03:49 +0530

    first commit

---
+++ leapyear/src/leapyear/leap.java
@@ -0,0 +1,13 @@
+package leapyear;
+
+public class leap {
+	void display()
+	{
+		System.out.println("Leap year");
+	}
+  public static void main(String[] args)
+  {
+    leap l=new leap();
+    l.display();
+  }
+}
