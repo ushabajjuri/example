ff366b745409d967c5ddd3bc4f7adccb56a17744
seela <seela@SSREDDY>
2023-09-21 14:31:53 +0530

    second commit

--- leapyear/src/leapyear/leap.java
+++ leapyear/src/leapyear/leap.java
@@ -4,10 +4,13 @@ public class leap {
 	void display()
 	{
 		System.out.println("Leap year");
+	}void add()
+	{
+		System.out.println("add");
 	}
   public static void main(String[] args)
   {
     leap l=new leap();
     l.display();
-  }
+    l.add();  }
 }
